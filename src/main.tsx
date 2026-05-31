e5bd6749ae9af8d2ac5157a57240511835c806ce
TopVitamin <vitamin1927@outlook.com>
2026-05-31 14:23:50 +0000

    Fix: set Vite base & Router basename for subdirectory deploy

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/projects/vita-jxc-deepseek">
       <App />
     </BrowserRouter>
   </React.StrictMode>
